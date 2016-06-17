6643299106056a5efa33c431c40b8d80eb2e42ab
jjp222 <jjp222@saramin.co.kr>
2016-06-17 18:55:02 +0900

    커밑 누락분..  console.log 변경

--- book_react.js/app/jsx/action/TweetActionCreators.jsx
+++ book_react.js/app/jsx/action/TweetActionCreators.jsx
@@ -2,7 +2,7 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 
 function receiveTweet(tweet) {
 
-    console.log('액션이 실행 => 디스패치 => 스토어로 tweet을 받아옴.');
+    console.log('액션실행 => 디스패치 => 스토어로 tweet을 받아옴.');
 
     var action = {
         type : 'receive_tweet',
