b2b556443510b4e15a5be97dae36bf368fc074d3
jaepil <jjp372c@gmail.com>
2016-07-01 16:27:32 +0900

    4장 까지 학습중

--- book_proReact.js/source/app/KanbanBoardContainer.js
+++ book_proReact.js/source/app/KanbanBoardContainer.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import update from 'react-addons-update';
+import {throttle} from './utils';
 import KanbanBoard from './KanbanBoard';
+
+// polyfill
 import 'babel-polyfill';
 import 'whatwg-fetch';
+
 
 // 서버를 로컬에서 실행하는 경우 기본 URL은 localhost:3000 이며, 로컬 서버에서는 권한 부여 헤더가 필요 없다.
 const API_URL = 'http://kanbanapi.pro-react.com';
@@ -16,7 +21,9 @@ class KanbanBoardContainer extends React.Component {
         super(...arguments);
         this.state = {
             cards : []
-        }
+        };
+        this.updateCardStatus = throttle(this.updateCardStatus.bind(this));
+        this.updateCardPosition = throttle(this.updateCardPosition.bind(this));
     }
 
     componentDidMount() {
@@ -65,13 +72,14 @@ class KanbanBoardContainer extends React.Component {
                 throw new Error("Server reqponse wasn`t OK");
             }
         })
-        .then( data => {
+        .then( res => {
             // 서버가 새로운 태스크를 추가하는 이용,
             // 확정 ID를 반환하면 리액트에서 ID를 업데이트 한다.
-            newTask.id = req.id;
+            newTask.id = res.id;
             this.setState({cards:nextState});
         })
         .catch( error => {
+            console.log(error);
             this.setState(prevState);
         })
         ;
@@ -98,8 +106,8 @@ class KanbanBoardContainer extends React.Component {
         fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
             method : 'delete',
             headers : API_HEADERS
-        }).then( req => {
-            if ( !req.ok ) {
+        }).then( res => {
+            if ( !res.ok ) {
                 // 서버에 응답이 정상이 아닌경우, 오류를 생성해 UI 변경을 되돌린다.
                 throw new Error("Server reqponse wasn`t OK");
             }
@@ -118,12 +126,12 @@ class KanbanBoardContainer extends React.Component {
         let cardIndex = this.state.cards.findIndex( card => card.id == cardId );
 
         // 태스트의 done 값에 대한 참조를 저장한다.
-        let newDonValue;
+        let newDoneValue;
         // $apply 명열을 이용해 done 값을 현재와 반대로 변경한다.
         let nextState = update(this.state.cards, {
             [cardIndex] : {
                 tasks : {
-                    [taskIndx] : {
+                    [taskIndex] : {
                         done : { $apply : done => {
                                 newDoneValue = !done;
                                 return newDoneValue;
@@ -141,13 +149,71 @@ class KanbanBoardContainer extends React.Component {
         fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
             method : 'put',
             headers : API_HEADERS,
-            body : JSON.stringify({done:newDeonValue})
+            body : JSON.stringify({done:newDoneValue})
         }).then( req => {
             if ( !req.ok ) {
                 throw new Error("Server reqponse wasn`t OK");
             }
         }).catch( error => {
             this.setState(prevState);
+        });
+    }
+
+    updateCardStatus(cardId, listId) {
+        let cardIndex = this.state.cards.findIndex( card => card.id == cardId );
+        let card = this.state.cards[cardIndex];
+
+        if ( card.status !== listId ) {
+            this.setState( update(this.state, {
+                cards : {
+                    [cardIndex] : {
+                        status : {$set:listId}
+                    }
+                }
+            }));
+        }
+    }
+
+    updateCardPosition(cardId, afterId) {
+        if ( cardId !== afterId ) {
+            let cardIndex = this.state.cards.findIndex( card => card.id == cardId );
+            let card = this.state.cards[cardIndex];
+            let afterIndex = this.state.cards.findIndex( card => card.id == afterId );
+
+            // splice를 이용해 카드를 제거한 후 새로운 인덱스 위치로 삽입한다.
+            this.setState(update(this.state, {
+                cards: {
+                    $splice : [
+                        [cardIndex, 1], [afterIndex, 0, card]
+                    ]
+                }
+            }))
+        }
+    }
+
+    persistCardDrag(cardId, status) {
+        let cardIndex = this.state.cards.findIndex( card => card.id == cardId );
+        let card = this.state.cards[cardIndex];
+
+        fetch(`${API_URL}/cards/${cardId}`, {
+            method : 'put',
+            headers : API_HEADERS,
+            body : JSON.stringify({status:card.status, row_order_position:cardIndex})
+        }).then( res => {
+            if ( !res.ok ) {
+                throw new Error("Server reqponse wasn`t OK");
+            }
+        }).catch( error => {
+            console.error("error : " + error);
+            this.setState(
+                update(this.state, {
+                    cards : {
+                        [cardIndex] : {
+                            status : {$set:status}
+                        }
+                    }
+                })
+            );
         });
     }
 
@@ -159,6 +225,11 @@ class KanbanBoardContainer extends React.Component {
                     toggle : this.toggleTask.bind(this),
                     delete : this.deleteTask.bind(this),
                     add : this.addTask.bind(this)
+                }}
+                cardCallbacks={{
+                    updateStatus:this.updateCardStatus,
+                    updatePosition:this.updateCardPosition,
+                    persistCardDrag:this.persistCardDrag.bind(this)
                 }}
             />
         );
